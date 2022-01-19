f6cc823fc3dfc7818b9a2d1fa3527e187759a09d
Stepan Dulka <fotosteuge@gmail.com>
2022-01-19 12:30:57 +0200

    Change true & false with 1 & 0

--- src/com/cursor/LogicalOpTable.java
+++ src/com/cursor/LogicalOpTable.java
@@ -4,9 +4,11 @@ public class LogicalOpTable {
 
     public static void main(String args[]) {
 
-        boolean p, q;
+        boolean p;
+        boolean q;
+
         System.out.println(" P  \t Q  \t  AND \t  OR \t  XOR \t NOT ");
-        System.out.println(" P  \t  Q \t(p & q)\t(p | q)\t(p ^ q)\t(!p)");
+        System.out.println(" P  \t Q  \t(p & q)\t(p | q)\t(p ^ q)\t(!p)");
         p = true;
         q = true;
         System.out.print(p + "\t" + q + "\t");
@@ -15,7 +17,7 @@ public class LogicalOpTable {
         p = true;
         q = false;
         System.out.print(p + "\t" + q + "\t");
-        System.out.print((p & q) + "\t" + (p | q) + "\t ");
+        System.out.print((p & q) + "\t" + (p | q) + "\t");
         System.out.println((p ^ q) + "\t" + (!p));
         p = false;
         q = true;
@@ -26,6 +28,39 @@ public class LogicalOpTable {
         q = false;
         System.out.print(p + "\t" + q + "\t");
         System.out.print((p & q) + "\t" + (p | q) + "\t");
-        System.out.println((p ^ q) + "\t " + (!p));
+        System.out.println((p ^ q) + "\t" + (!p));
+
+
+        int p1;
+        int q1;
+
+        System.out.println("\n");
+
+        System.out.println(" P  \t Q  \t  AND \t  OR \t  XOR \t NOT ");
+        System.out.println(" P  \t Q  \t(p & q)\t(p | q)\t(p ^ q)\t(!p)");
+        p1 = 1;
+        q1 = 1;
+        System.out.print(" " + p1 + "\t     " + q1 + "\t     ");
+        System.out.print((p1 & q1) + "\t      " + (p1 | q1) + "\t      ");
+        System.out.println((p1 ^ q1) + "\t      " + (p1 = 0));
+        p1 = 1;
+        q1 = 0;
+        System.out.print(" " + p1 + "\t     " + q1 + "\t     ");
+        System.out.print((p1 & q1) + "\t      " + (p1 | q1) + "\t      ");
+        System.out.println((p1 ^ q1) + "\t      " + (p1 = 0));
+        p1 = 0;
+        q1 = 1;
+        System.out.print(" " + p1 + "\t     " + q1 + "\t     ");
+        System.out.print((p1 & q1) + "\t      " + (p1 | q1) + "\t      ");
+        System.out.println((p1 ^ q1) + "\t      " + (p1 = 1));
+        p1 = 0;
+        q1 = 0;
+        System.out.print(" " + p1 + "\t     " + q1 + "\t     ");
+        System.out.print((p1 & q1) + "\t      " + (p1 | q1) + "\t      ");
+        System.out.println((p1 ^ q1) + "\t      " + (p1 = 1));
     }
 }
+/* Попытайтесь видоизменить программу так, чтобы вместо логических значений
+true и false отображались значения 1 и О. Это потребует больших
+усилий, чем кажется на первый взгляд!
+ */
